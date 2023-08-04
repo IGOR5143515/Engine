eb6850fcf7080387ec6b364f7bb2cfbd7a3601ce
IGOR <pepsidead@gmail.com>
2023-08-04 11:32:54 +0300

    draw working wersion

--- SFML/Main.cpp
+++ SFML/Main.cpp
@@ -5,68 +5,76 @@
 
 using namespace sf;
 
+class test {
+private:
+	Texture texture;
+	Sprite sprite;
+public:
+	test(std::string link) {
+		texture.loadFromFile(link);
+		sprite.setTexture(texture);
+		sprite.setPosition(1, 1);
+	}
 
+	Sprite getSprite() { return sprite; }
+
+
+};
+void foo(std::vector<test> arr,RenderWindow &w) {
+	for (size_t i = 0; i < arr.size(); i++)
+	{
+		w.draw(arr[i].getSprite());
+		
+	}
+}
 int main()
 {
 	
 	RenderWindow w(sf::VideoMode(1280,720), "Game");
-    bool isMove = false;
-	Texture texture;
-	texture.loadFromFile("C:/Users/User/source/repos/SFML/resorses/img2.png.png");
-	Sprite sprite;
+	test q("C:/Users/User/source/repos/SFML/resorses/img3.jpg");
+	test two("C:/Users/User/source/repos/SFML/resorses/img2.png.png");
 	
-	sprite.setTexture(texture);
-	sprite.setScale(0.25,0.25);
+	std::vector<test>arr{ two,q };
+
+
 	ReadAndWrite x("1.txt","2.txt");
-  
-   
-    x.read(sprite);
 	
-		
+    
+	
 	while (w.isOpen()) {
-       
-       
-        Vector2i pixelPos = Mouse::getPosition(w);
-        Vector2f pos = w.mapPixelToCoords(pixelPos);
+		
+        
+		w.clear();
+		foo(arr,w);
+		w.display();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         sf::Event event;
         while (w.pollEvent(event))
         {
-
             if (event.type == sf::Event::Closed)
                 w.close();
-
-            if (event.type == Event::MouseButtonPressed)
-                if (event.key.code == Mouse::Left)
-                    if (sprite.getGlobalBounds().contains(pos.x, pos.y)) {
-
-
-                        isMove = true;
-                    }
-
-            if (event.type == Event::MouseButtonReleased)
-                if (event.key.code == Mouse::Left) {
-                    std::cout << sprite.getPosition().x << " " <<
-                        sprite.getPosition().y << std::endl;
-                    
-                    x.write(sprite);
-                    isMove = false;
-
-                }
-
-
-            if (isMove == true) {
-                sprite.setPosition(Vector2f(pos.x, pos.y));
-            }
-
-
         }
-
-
-
-		w.clear();
-		w.draw(sprite);
-		w.display();
 	}
-
-
 }
