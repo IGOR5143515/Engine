0e85f982a05f4f81f946f82898b4a55420a82a5a
IGOR <pepsidead@gmail.com>
2023-08-04 13:05:52 +0300

    edits

--- SFML/Main.cpp
+++ SFML/Main.cpp
@@ -1,1 +1,13 @@
-﻿
+﻿#include <SFML/Graphics.hpp>
+#include<iostream>
+#include<vector>
+#include"ReadAndWrite.h"
+#include"Engine.h"
+
+
+int main()
+{
+	sf::RenderWindow w(sf::VideoMode(1280,720),"Game");
+	Engine game;
+	game.Start(w);
+}
