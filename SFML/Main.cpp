454c6691f73db450349214dc996a8e624a42c938
IGOR <UkraininanCradt@gmail.com>
2023-08-08 19:46:03 +0300

    first release version

--- SFML/Main.cpp
+++ SFML/Main.cpp
@@ -11,9 +11,9 @@ int main()
 	sf::RenderWindow w(sf::VideoMode(1280,720),"Game");
 	
 	test t;
-t.addSprite("C:/Users/IGOR/source/repos/Engine/resorses/img2.png.png",
+t.addSprite("resorses/img2.png.png",
 		"1.txt", "2.txt");
-t.addSprite("C:/Users/IGOR/source/repos/Engine/resorses/img1.png.png",
+t.addSprite("resorses/img1.png.png",
 	"4.txt", "3.txt");
 	
 	t.start(w);
